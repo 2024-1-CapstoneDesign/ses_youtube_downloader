15e8d650a338b8c972dd1adda4bf96f0734bb1c4
hayeongKo <polarpheno@icloud.com>
2024-06-02 00:27:51 +0900

    git 추가

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class User(AbstractUser):
+    pass
+
+    @staticmethod
+    def get_user_or_none_by_email(email):
+        try:
+            return User.objects.get(email=email)
+        except Exception:
+            return None
