678574442bc070c0aebc4576c9077d5e7dd7f851
hayeongKo <polarpheno@icloud.com>
2024-06-02 00:21:29 +0900

    [FEAT] token 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,16 @@
 from .views import *
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
-    path("google/login/", google_login, name="google_login"),
-    path("google/callback/", google_callback, name="google_callback"),
+    # path("google/login/", google_login, name="google_login"),
+    path("google/callback/", google_callback, name="google_callㄴback"),
+
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
