272d12107141a978e2fa4f6a690cbe6175ffe268
hayeongKo <polarpheno@icloud.com>
2024-06-02 01:34:15 +0900

    code 추출

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("google/login/", google_login, name="google_login"),
-    path("google/callback/", google_callback, name="google_callㄴback"),
+    path("google/callback/", google_callback, name="google_callback"),
+    path("google/callback/re", google_callback, name="google_callback"),
+
 
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
--- accounts/views.py
+++ accounts/views.py
@@ -35,6 +35,56 @@ def google_login(request):
 def google_callback(request):
     code = request.GET.get("code")  # Query String 으로 넘어옴
 
+    # token_req = requests.post(
+    #     f"https://oauth2.googleapis.com/token?client_id={GOOGLE_CLIENT_ID}&client_secret={GOOGLE_SECRET}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}"
+    # )
+    # token_req_json = token_req.json()
+    # error = token_req_json.get("error")
+    #
+    # if error is not None:
+    #     raise JSONDecodeError(error)
+    #
+    # google_access_token = token_req_json.get('access_token')
+    #
+    # email_response = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={google_access_token}")
+    # res_status = email_response.status_code
+    #
+    # if res_status != 200:
+    #     return JsonResponse({'status': 400, 'message': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # email_res_json = email_response.json()
+    #
+    # serializer = OAuthSerializer(data=email_res_json)
+    # if serializer.is_valid(raise_exception=True):
+    #     user = serializer.validated_data["user"]
+    #     access_token = serializer.validated_data["access_token"]
+    #     refresh_token = serializer.validated_data["refresh_token"]
+    #
+    #     # 사용자 인증 설정
+    #     login(request, user)
+    #
+    #     res = JsonResponse(
+    #         {
+    #             "user": {
+    #                 "id": user.id,
+    #                 "email": user.email,
+    #             },
+    #             "message": "login success",
+    #             "token": {
+    #                 "access_token": access_token,
+    #                 "refresh_token": refresh_token,
+    #             },
+    #         },
+    #         status=status.HTTP_200_OK,
+    #     )
+    #     res.set_cookie("access-token", access_token, httponly=True)
+    #     res.set_cookie("refresh-token", refresh_token, httponly=True)
+    #     return res
+    # return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def google_callback(request):
+    code = request.GET.get("code")  # Query String 으로 넘어옴
+
     token_req = requests.post(
         f"https://oauth2.googleapis.com/token?client_id={GOOGLE_CLIENT_ID}&client_secret={GOOGLE_SECRET}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}"
     )
