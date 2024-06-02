6e84bcdb8c6a4959f609d901284ef1f4ae894f6d
hayeongKo <polarpheno@icloud.com>
2024-06-02 22:54:27 +0900

    cors_allowed_origin 수정

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 ROOT_URLCONF = "config.urls"
 
