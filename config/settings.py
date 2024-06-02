f48245e45bb8cf044355e7a09da3bbf5afd9a070
hayeongKo <polarpheno@icloud.com>
2024-06-03 02:47:42 +0900

    allowed_host 수정

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "soundeffect-search.p-e.kr", "soundeffect-search.p-e.kr:8000"]
 
 
 # Application definition
